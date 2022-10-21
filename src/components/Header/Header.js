7977bcbb895ab29359bfce2f9335dd2089b88e42
AZHAR <a.uddin4321@gmail.com>
2022-10-21 19:15:55 +0600

    ema-john-shipping

---
+++ src/components/Header/Header.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import logo from '../../images/Logo.svg'
+import './Header.css'
+const Header = () => {
+    return (
+        <nav className='header'>
+            <img src={logo} alt="" />
+            <div>
+                <a href="3">Home</a>
+                <a href="#">Orders</a>
+                <a href="#">Inventory</a>
+                <a href="#">Login</a>
+            </div>
+        </nav>
+    );
+};
+
+export default Header;
